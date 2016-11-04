78f7222e5dc9a0ca9b8459567778ab4f3083fd82
Hector Ocampo <hector-2603@hotmail.com>
2016-11-04 13:49:31 -0500

    volviendo a sqlite en desarrollo

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161104035111) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -30,8 +27,8 @@ ActiveRecord::Schema.define(version: 20161104035111) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "rol"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
