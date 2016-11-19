ea2bb36f2da378d112d7e2d6cd07358d3afb20af
Hector Ocampo <hector-2603@hotmail.com>
2016-11-19 00:32:29 -0500

    imagen de pefil

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,9 @@ before_action :configure_account_update_params, only: [:update]
   # end
 
   # POST /resource
-  def create
-    super
-  end
+  #def create
+  #  super
+  #end
 
   # GET /resource/edit
   # def edit
@@ -40,12 +40,12 @@ before_action :configure_account_update_params, only: [:update]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :rol])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :rol,:avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :rol])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :rol,:avatar])
   end
 
   # The path used after sign up.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
+
+	has_attached_file :avatar, styles: {peque: "50x50", media: "100x100", grande: "200x200"}
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+	#do_not_validate_attachment_file_type :avatar
+
+
 end
