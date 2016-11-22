ffd0601919441fad24ea4b333be02751300782ca
AndresRiascos <carlos.a.riascos@correounivalle.edu.co>
2016-11-22 13:10:48 -0500

    Carousel con Noticias

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  respond_to :html
   def index
+  	 @reports = Report.where(active: true)
+  	 respond_with(@usuarios)
   end
 end
