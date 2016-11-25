e8580313ab9fcce6bdefa1b5fde8a0a69d9aff07
AndresRiascos <carlos.a.riascos@correounivalle.edu.co>
2016-11-25 11:33:25 -0500

    Cambios en Cursos

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123060841) do
+ActiveRecord::Schema.define(version: 20161125150933) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.date     "date_end"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "user_id"
+    t.text     "description"
+    t.integer  "code"
+    t.boolean  "active",      default: false
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
