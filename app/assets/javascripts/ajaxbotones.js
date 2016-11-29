db0dd569f9b673e9e036fabaa552109a0e18dd0b
Hector Ocampo <hector-2603@hotmail.com>
2016-11-29 14:10:28 -0500

    Ya se pueden asignar varios estudiantes

--- app/assets/javascripts/ajaxbotones.js
+++ app/assets/javascripts/ajaxbotones.js
@@ -4,3 +4,13 @@ function activarUsuario(id){
 		$("#"+id).remove();
 	});
 }
+
+function agregarSelect2Estudiante(){
+	if (typeof estudiantes != "undefined"){
+		var sel = $('<select>').appendTo('#contenedorEstudiantes');
+		$(estudiantes).each(function() {
+		 sel.append($("<option>").attr('value',this.id).text(this.name));
+		});
+		sel.select2();
+	}
+}
