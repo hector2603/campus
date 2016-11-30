6d176f40d9dd27098d820495fdef80232f5d669e
Hector Ocampo <hector-2603@hotmail.com>
2016-11-30 00:56:27 -0500

    arreglar una consulta

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   respond_to :html
   def index
   	 @reports = Report.where(active: true)
-  	 @courses = Course.where("user_id", current_user.id)
+  	 @courses = Course.where(user_id: current_user.id)
   	 respond_with(@usuarios,@courses)
   end
 end
