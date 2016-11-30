1ba8654f333047ff0bc4a5a1da742d15cb77426f
Hector Ocampo <hector-2603@hotmail.com>
2016-11-30 00:58:35 -0500

    otra consulta.... arreglada

--- app/controllers/course_users_controller.rb
+++ app/controllers/course_users_controller.rb
@@ -2,7 +2,7 @@ class CourseUsersController < ApplicationController
 	before_action :authenticate_user!
 	before_action :es_profesor?, only:[:new, :create,:edit,:update]
 	def index
-		@courses = Course.where("user_id", current_user.id)
+		@courses = Course.where(user_id: current_user.id)
 	end
 
 	def show
