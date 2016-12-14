4ce3675a97505584dcf6ac113fd4c44c942c5408
Hector Ocampo <hector-2603@hotmail.com>
2016-12-14 11:38:12 -0500

    crear admin

--- db/migrate/20161115032557_add_fiel_active_user.rb
+++ db/migrate/20161115032557_add_fiel_active_user.rb
@@ -1,7 +1,6 @@
 class AddFielActiveUser < ActiveRecord::Migration[5.0]
   def change
   	add_column :users, :active, :boolean, default: false
-  	User.create name: 'admin', email: 'admin@gmail.com', rol: '1', password: '123456', active: true
   end
 
 end
--- db/migrate/20161205024200_create_comments.rb
+++ db/migrate/20161205024200_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
-  	 User.create(name: 'admin', email: 'admin@gmail.com', rol: '1', password: '123456', active: true)
+  	User.create(name: 'admin', email: 'admin@gmail.com', rol: '1', password: '123456', active: true)
     create_table :comments do |t|
       t.references :user, foreign_key: true
       t.references :blog, foreign_key: true
